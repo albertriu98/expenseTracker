14bd3d09e9f658dc1ccef96963670c032633a384
Albert Riu <a.riu@externals.phoenixgroup.eu>
2026-02-20 23:11:08 +0100

    Add event

--- src/accounting/domain/account/account.py
+++ src/accounting/domain/account/account.py
@@ -1,19 +1,31 @@
 from src.accounting.domain.accountId import AccountId
 from src.accounting.domain.shared.domain_exceptions import InsufficientFundsException
+from src.accounting.domain.money import Money
+from src.accounting.domain.events import TransactionCommitted
 
 class Account:
-    def __init__(self, currentBalance: float):
-        self.accountId = AccountId.new()
-        self._currentBalance = currentBalance
-        
-    
-    def deposit(self, amount):
-        self._current_balance += amount
+    def __init__(self, initBalance: decimal, currency: str):
+        self._accountId = AccountId.new()
+        self._currency = currency
+        self._currentBalance = initBalance
+
+    def deposit(self, amount: Money):
+        if self._currency != amount.currency :
+            raise InvalidCurrency("Transaction currency does not match with Account currency")
+        self._current_balance += amount.amount
+        TransactionCommitted()
         return "Deposit correctly performed"
 
-    
-    def withdraw(self, amount):
+    def withdraw(self, amount : Money):
         if self._currentBalance < amount :
             raise InsufficientFundsException("Not enough balance to withdraw.")
+        else if self._currency != amount.currency :
+            raise InvalidCurrency("Transaction currency does not match with Account currency")
+        self._currentBalance -= amount.amount
+        TransactionCommitted()
+
+    @classmethod
+    def openAccount(cls, initAmount: Money)
+        return cls(Account(initBalance=initAmount.amount, currency=initAmount.currency))
 
     
