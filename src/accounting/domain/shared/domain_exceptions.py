677aea070db22e6fdb3a08f67ec08b8220e08141
Albert Riu <arivermar@gmail.com>
2026-02-20 23:11:08 +0100

    Add event

--- src/accounting/domain/shared/domain_exceptions.py
+++ src/accounting/domain/shared/domain_exceptions.py
@@ -1,3 +1,7 @@
 class InsufficientFundsException(Exception):
     """Raised when an account does not have enough balance for a withdrawal."""
     pass
+
+class InvalidCurrency(Expetion):
+    """Raised when Account currency does not match transaction currency"""
+    pass
