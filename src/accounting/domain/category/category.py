f85d73d4ff10d638650c4403a9c8dfbd7b239307
Albert Riu <arivermar@gmail.com>
2026-02-23 08:07:06 +0100

    Create budgeting context. create entities for it.

--- src/accounting/domain/category/category.py
+++
@@ -1,6 +0,0 @@
-class Category :
-    def __init__(self, name: str, transactions: list(Transaction), userId: userId):
-        self._id = 
-        self._name = name
-        self._transactions = transactions
-        self._userId = userId
