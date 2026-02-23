c073c958762746b72e41d2727f80f4f9c6dd043c
Albert Riu <a.riu@externals.phoenixgroup.eu>
2026-02-23 08:07:14 +0100

    Create budgeting context. create entities for it.

---
+++ src/budgeting/budget/budget.py
@@ -0,0 +1,9 @@
+class Budget:
+    def __init__(self, accountId: accountId, name: str, moneyLImit: Money, timeRange: timeRange, categoryId: categoryId):
+        self._id = BudgetId.new()
+        self._accountId =
+        self._name = name
+        self._moneyLimit = moneyLImit
+        self._actualMoney = 0
+        self._timeRange = timeRange
+        self._categoryId = categoryId
