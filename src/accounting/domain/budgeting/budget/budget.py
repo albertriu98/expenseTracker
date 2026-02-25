772a78dfb7361acba7972d523827b3f2b76d3ded
Albert Riu <arivermar@gmail.com>
2026-02-25 08:11:38 +0100

    Keep up with buget

--- src/accounting/domain/budgeting/budget/budget.py
+++ src/accounting/domain/budgeting/budget/budget.py
@@ -7,3 +7,40 @@ class Budget:
         self._actualMoney = 0
         self._timeRange = timeRange
         self._categoryId = categoryId
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def accountId(self):
+        return self._accountId
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def moneyLimit(self):
+        return self._moneyLimit
+    
+    @property
+    def moneyLimit(self):
+        return self._moneyLimit
+    
+    @property
+    def actualMoney(self):
+        return self._actualMoney
+    
+    @property
+    def timeRange(self):
+        return self._categoryId
+    
+    def accountTransaction(self):
+        # Receive transaction (context mapping) and account it to actualMoney
+        pass
+
+
+    @classmethod
+    def createBudget(cls, accountId, name, moneyLImit, timeRange, categoryId):
+        return cls(accountId, name, moneyLImit, timeRange, categoryId)
