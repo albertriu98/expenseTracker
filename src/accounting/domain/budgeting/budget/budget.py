a9e334bed015a47fb1e9ea8b151117e95e0d193b
Albert Riu <arivermar@gmail.com>
2026-02-25 08:12:35 +0100

    Keep up with buget

--- src/accounting/domain/budgeting/budget/budget.py
+++ src/accounting/domain/budgeting/budget/budget.py
@@ -1,7 +1,7 @@
 class Budget:
     def __init__(self, accountId: accountId, name: str, moneyLImit: Money, timeRange: timeRange, categoryId: categoryId):
         self._id = BudgetId.new()
-        self._accountId =
+        self._accountId = accountId
         self._name = name
         self._moneyLimit = moneyLImit
         self._actualMoney = 0
