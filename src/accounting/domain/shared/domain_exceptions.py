ea4ef1ac24543b998e73b4e66c2e0d3b06623c63
Albert Riu <arivermar@gmail.com>
2026-03-08 19:29:48 +0100

    Reorganize accounting domain, add some guards with setters

--- src/accounting/domain/shared/domain_exceptions.py
+++
@@ -1,7 +0,0 @@
-class InsufficientFundsException(Exception):
-    """Raised when an account does not have enough balance for a withdrawal."""
-    pass
-
-class InvalidCurrencyException(Expetion):
-    """Raised when Account currency does not match transaction currency"""
-    pass
--- src/accounting/domain/transaction/transactionType.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from enum import Enum
-
-@dataclass(frozen=True)
-class TransactionType(Enum):
-    INCOME = "income"
-    EXPENSE = "expense"
-
-
