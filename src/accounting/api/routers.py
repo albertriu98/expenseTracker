284b7de2003a019394e477b02a32be27de5e1ff3
Albert Riu <arivermar@gmail.com>
2026-05-13 08:21:47 +0200

    fix depends and session

--- src/accounting/api/routers.py
+++ src/accounting/api/routers.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends
 from typing import List
 from src.accounting.application.services import AccountHandler
 from src.accounting.application.commands import CreateAccountCommand, CommitTransactionCommand
-from src.accounting.infrastructure.database.session import get_session, SessionDep
+from src.accounting.infrastructure.database.session import SessionDep
 
 
 router = APIRouter(
@@ -14,14 +14,14 @@ router = APIRouter(
 def get_handler(session: SessionDep):
     return AccountHandler(session)
 
-@router.post("/create_account", response_model=List[str])
-async def create_account(amount: float, currency: str, session: SessionDep, handler: AccountHandler = Depends(get_handler)):
+@router.post("/create_account")
+async def create_account(amount: float, currency: str, handler: AccountHandler = Depends(get_handler)):
     account_id = handler.create_account(CreateAccountCommand(amount=amount, currency=currency))
     return {"accountId": account_id}
 
 
 @router.post("/commit_transaction")
-async def commit_transaction(accountId: str, amount: float, currency: str, description: str, category: str, transactionType: str, session: SessionDep, handler: AccountHandler = Depends(get_handler)):
+async def commit_transaction(accountId: str, amount: float, currency: str, description: str, category: str, transactionType: str, handler: AccountHandler = Depends(get_handler)):
     current_balance = handler.commit_transaction(CommitTransactionCommand(
         account_id=accountId,
         amount=amount,
